04307eab21668548e4589b58d2df55a66317a159
e2380426 <e2380426@ceng.metu.edu.tr>
2022-12-11 16:38:52 +0300

    orthonormal created

--- code_template/Main.cpp
+++ code_template/Main.cpp
@@ -17,8 +17,16 @@ Matrix4 compositeAll(vector<Matrix4> tranformsAll){
     return result;
 }
 
+// creating orthonormal tranformation matrix
 Matrix4 M_orth(double left, double right, double bottom, double top, double near, double far){
-    
+    Matrix4 result=getIdentityMatrix();
+    result.val[0][0]=2/(right-left);
+    result.val[1][1]=2/(top-bottom);
+    result.val[2][2]=-2/(far-near);
+    result.val[0][3]=-(right+left)/(right-left);
+    result.val[1][3]=-(top+bottom)/(top-bottom);
+    result.val[2][3]=-(far+near)/(far-near);
+    return result;
 }
 
 
